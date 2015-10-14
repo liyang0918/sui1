f31bdb552ad8198c128a6abe7b013a175c8cc7b2
root <root@hadoop1.(none)>
2015-10-14 08:12:04 +0800

    2015年 10月 14日 星期三 08:12:04 CST

--- test.php
+++ test.php
@@ -5,6 +5,10 @@
  * Date: 15-9-16
  * Time: ÉÏÎç11:25
  */
-$str = "a";
 
-echo intval($str);
+$arr = file("/home/bbs/mail/M/mitbbs/M.1440397542_2.Z0");
+
+foreach ($arr as $key=>$value) {
+    echo "[$key]   $value<br />";
+}
+
